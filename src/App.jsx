b0708844298939737a396b52bb9e41dc983437b3
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-02-29 17:45:43 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button> Click Me </Button>
+    </>
+  )
+}
+
+export default App
