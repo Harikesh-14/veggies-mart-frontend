a884e5e252ef93df1432c0d28a8041ae212cf7f2
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-03-02 11:17:39 +0530

    TopHeader CSS improved

--- src/components/containers/TopHeader/TopHeader.jsx
+++ src/components/containers/TopHeader/TopHeader.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
 
 import './TopHeader.css'
 
 function TopHeader() {
-  return (
-    <div className='flex flex-row'>
-        <div>
-            <ul className='flex flex-row'>
-                <li>
-                    <Link to={'/account'}>Account</Link>
-                </li>
-                <li>
-                    <Link to={'/track-order'}>Track Order</Link>
-                </li>
-                <li>
-                    <Link to={'/support'}>Support</Link>
-                </li>
-            </ul>
+    return (
+        <div className='topHeader flex justify-center items-center'>
+            <div className="topHeaderMainContainer">
+                <div className='leftSide'>
+                    <ul className='flex flex-row'>
+                        <li className='tab cursor-pointer'>
+                            <Link to={'/account'}>Account</Link>
+                        </li>
+                        <li className='tab cursor-pointer'>
+                            <Link to={'/track-order'}>Track Order</Link>
+                        </li>
+                        <li className='tab cursor-pointer'>
+                            <Link to={'/support'}>Support</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='rightSide'>
+                    <ul className='flex flex-row'>
+                        <li className='font-semibold'><FontAwesomeIcon icon={faPhone} /> +91 9319958749</li>
+                        <li className="font-semibold"><FontAwesomeIcon icon={faAt} /> ranjansinhaharikesh@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className='rightSide'>
-            <ul className='flex flex-row'>
-                <li>+91 9319958749</li>
-                <li>ranjansinhaharikesh@gmail.com</li>
-            </ul>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default TopHeader
